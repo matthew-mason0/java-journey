82ba618871db1d1c0aa0d244c08297b162370100
Matthew Mason <matthewmason2004@yahoo.co.uk>
2026-09-01 20:21:12 +0100

    Test hospital class

--- vscode/oop-test/src/App.java
+++ vscode/oop-test/src/App.java
@@ -1,9 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Patient patient1 = new Patient("Matthew", 21, 5);
+        Patient patient1 = new Patient("Matthew", 21, 6);
         patient1.displayInfo();
+
+        Patient patient2 = new Patient("Bethan", 18, 8);
 
         Doctor doctor1 = new Doctor("Wang", "Brain");
         doctor1.examine(patient1);
+
+        Hospital hospital1 = new Hospital("St. Mary\'s");
+        hospital1.addPatient(patient1);
+        hospital1.addPatient(patient2);
+
+        hospital1.displayPatients();
     }
 }
