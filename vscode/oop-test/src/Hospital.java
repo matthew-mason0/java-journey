1ec8e0663e547de2981181e992bee69708f95688
Matthew Mason <matthewmason2004@yahoo.co.uk>
2026-09-01 20:15:39 +0100

    Add methods to hospital class

--- vscode/oop-test/src/Hospital.java
+++ vscode/oop-test/src/Hospital.java
@@ -1,14 +1,37 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Hospital {
     private String name;
-    private ArrayList<Patient> patientList;
+    private List<Patient> patientList;
 
     public Hospital() {
-        
-
+        patientList = new ArrayList<>();
     }
+
     public String getName() {
         return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPatientCount() {
+        return patientList.size();
+    }
+
+    public void addPatient(Patient patient) {
+        patientList.add(patient);
+    }
+    public void removePatient(Patient patient) {
+        patientList.remove(patient);
+    }
+
+    public void displayPatients() {
+        System.out.println("---Patient List---");
+        int i = 0;
+        for (Patient patient : patientList) {
+            System.out.println("Patient " + i + ": " + patient.getName() + ", age " + patient.getAge());
+        }
     }
 }
