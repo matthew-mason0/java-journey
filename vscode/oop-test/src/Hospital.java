e66ae73cf4f2282267619b89d985eda16072350c
Matthew Mason <matthewmason2004@yahoo.co.uk>
2026-09-01 19:14:15 +0100

    Add Hospital class

---
+++ vscode/oop-test/src/Hospital.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Hospital {
+    private String name;
+    private ArrayList<Patient> patientList;
+
+    public Hospital() {
+        
+
+    }
+    public String getName() {
+        return name;
+    }
+}
